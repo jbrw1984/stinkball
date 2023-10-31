554562621e6f256db653e44c98e972dcc39ae287
Kobe Yang <kobeyang160@gmail.com>
2023-10-31 00:35:35 -0700

    Added NFL team interface

--- api/src/test/users.test.ts
+++ api/src/test/users.test.ts
@@ -143,55 +143,6 @@ describe('Testing Users', () => {
   });
 
 
-  // describe('[GET] /users/:id', () => {
-  //   it('response findOne user', async () => {
-  //     const userId = 1;
-
-  //     const usersRoute = new UserRoute();
-  //     const users = usersRoute.usersController.userService.users;
-
-  //     users.findByPk = jest.fn().mockReturnValue({
-  //       id: 1,
-  //       email: 'a@email.com',
-  //       password: await bcrypt.hash('q1w2e3r4!', 10),
-  //     });
-
-  //     (Sequelize as any).authenticate = jest.fn();
-  //     const app = new App([usersRoute]);
-  //     return request(app.getServer()).get(`${usersRoute.path}/${userId}`).expect(200);
-  //   });
-  // });
-
-
-  // describe('[PUT] /users/:id', () => {
-  //   it('response Update user', async () => {
-  //     const userId = 1;
-  //     const userData: CreateUserDto = {
-  //       email: 'test@email.com',
-  //       password: '1q2w3e4r!',
-  //     };
-
-  //     const usersRoute = new UserRoute();
-  //     const users = usersRoute.usersController.userService.users;
-
-  //     users.findByPk = jest.fn().mockReturnValue({
-  //       id: userId,
-  //       email: userData.email,
-  //       password: await bcrypt.hash(userData.password, 10),
-  //     });
-  //     users.update = jest.fn().mockReturnValue([1]);
-  //     users.findByPk = jest.fn().mockReturnValue({
-  //       id: userId,
-  //       email: userData.email,
-  //       password: await bcrypt.hash(userData.password, 10),
-  //     });
-
-  //     (Sequelize as any).authenticate = jest.fn();
-  //     const app = new App([usersRoute]);
-  //     return request(app.getServer()).put(`${usersRoute.path}/${userId}`).send(userData).expect(200);
-  //   });
-  // });
-
   describe('[DELETE] /users/:id', () => {
     it('response Delete user', async () => {
       const userId = 1;
