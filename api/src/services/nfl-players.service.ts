d7714a2de6ac343d6fc9cf5d032fd90bb5b72778
Avery Tribbett <apatribbett03@gmail.com>
2023-11-25 17:47:03 -0600

    Create NFL Player service

--- api/src/services/nfl-players.service.ts
+++ api/src/services/nfl-players.service.ts
@@ -2,6 +2,7 @@ import { Service } from 'typedi';
 import { DB } from '@database';
 import { HttpException } from '@/exceptions/httpException';
 import { NFLPlayer } from '@/interfaces/nfl-players.interface';
+import { NFLPlayerDTO } from '@/dtos/nfl-players.dto';
 
 @Service()
 export class NFLPlayerService {
@@ -15,5 +16,14 @@ export class NFLPlayerService {
     if (!findNFLPlayer) throw new HttpException(409, "NFL Player doesn't exist");
 
     return findNFLPlayer;
+  }
+
+  public async createNFLPlayer(nflPlayer: NFLPlayerDTO): Promise<NFLPlayer> {
+    // Check if team already exists.
+    const findNFLPlayer: NFLPlayer = await DB.NFLPlayers.findOne({ where: { player_name: nflPlayer.player_name, player_portrait: nflPlayer.player_portrait } });
+    if (findNFLPlayer) throw new HttpException(409, `This nfl player ${nflPlayer.player_name} already exists`);
+
+    const createNFLPlayer: NFLPlayer = await DB.NFLPlayers.create(nflPlayer);
+    return createNFLPlayer;
   }
 }
