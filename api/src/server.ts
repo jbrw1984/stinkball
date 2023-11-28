08a5e5a62ca1ec770ee8970aedeba50f86b16b08
Avery Tribbett <apatribbett03@gmail.com>
2023-11-28 12:18:44 -0600

    Added route to server file

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,9 +2,10 @@ import { App } from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
+import { NFLTeamRoute } from './routes/nfl-teams.route';
 
 ValidateEnv();
 
-const app = new App([new AuthRoute(), new UserRoute()]);
+const app = new App([new AuthRoute(), new UserRoute(), new NFLTeamRoute()]);
 
 app.listen();
