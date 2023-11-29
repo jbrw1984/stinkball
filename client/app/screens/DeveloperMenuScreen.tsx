616bb4c899e25b92825c50218c498e83416c128e
Kobe Yang <kobeyang160@gmail.com>
2023-11-28 20:27:10 -0800

    Fixed up colors file, added scrollview to developer menu

--- client/app/screens/DeveloperMenuScreen.tsx
+++ client/app/screens/DeveloperMenuScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { VStack, Button, ButtonText, ButtonIcon, AddIcon } from "@gluestack-ui/themed";
+import { VStack, Button, ButtonText, ButtonIcon, AddIcon, ScrollView } from "@gluestack-ui/themed";
 import { ScanFace, List, PlusCircle, Swords} from 'lucide-react-native';
 import { typography } from 'app/theme';
 import { AppStackParamList } from 'app/navigators/AppNavigator';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { colors } from "app/theme"
 
 type DeveloperMenuScreenNavigationProp = NativeStackNavigationProp<
   AppStackParamList,
@@ -21,62 +22,70 @@ export interface DeveloperMenuScreenProps {
 export function DeveloperMenuScreen(props: DeveloperMenuScreenProps) {
 
   return (
+    <ScrollView
+      backgroundColor={colors.background}
+      flex={1}
+    >
+      <VStack 
+        space='xl' 
+        marginTop={120} 
+        padding={20}
+        > 
 
-    <VStack space='xl' marginTop={120} padding={20}> 
+        <Button
+          size="md"
+          variant="solid"
+          action="primary"
+          marginHorizontal={20}
+          onPress={() => props.navigation.navigate('Welcome')}
+        >
+          <ButtonText fontFamily={typography.fonts.poppins.medium}>Login / Create Account Screen </ButtonText>
+          <ButtonIcon as={ScanFace} />
+        </Button>
 
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        marginHorizontal={20}
-        onPress={() => props.navigation.navigate('Welcome')}
-      >
-        <ButtonText fontFamily={typography.fonts.poppins.medium}>Login / Create Account Screen </ButtonText>
-        <ButtonIcon as={ScanFace} />
-      </Button>
+        <Button
+          size="md"
+          variant="solid"
+          action="primary"
+          marginHorizontal={20}
+          onPress={() => props.navigation.navigate('MatchList')}
+        >
+          <ButtonText fontFamily={typography.fonts.poppins.medium}>Match List Screen </ButtonText>
+          <ButtonIcon as={List} />
+        </Button>
 
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        marginHorizontal={20}
-        onPress={() => props.navigation.navigate('MatchList')}
-      >
-        <ButtonText fontFamily={typography.fonts.poppins.medium}>Match List Screen </ButtonText>
-        <ButtonIcon as={List} />
-      </Button>
+        <Button
+          size="md"
+          variant="solid"
+          action="primary"
+          marginHorizontal={20}
+        >
+          <ButtonText fontFamily={typography.fonts.poppins.medium}>Create Match Screen </ButtonText>
+          <ButtonIcon as={PlusCircle} />
+        </Button>
 
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        marginHorizontal={20}
-      >
-        <ButtonText fontFamily={typography.fonts.poppins.medium}>Create Match Screen </ButtonText>
-        <ButtonIcon as={PlusCircle} />
-      </Button>
+        <Button
+          size="md"
+          variant="solid"
+          action="primary"
+          marginHorizontal={20}
+        >
+          <ButtonText fontFamily={typography.fonts.poppins.medium}>Match Details Screen </ButtonText>
+          <ButtonIcon as={Swords} />
+        </Button>
 
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        marginHorizontal={20}
-      >
-        <ButtonText fontFamily={typography.fonts.poppins.medium}>Match Details Screen </ButtonText>
-        <ButtonIcon as={Swords} />
-      </Button>
+        <Button
+          size="md"
+          variant="solid"
+          action="primary"
+          marginHorizontal={20}
+        >
+          <ButtonText fontFamily={typography.fonts.poppins.medium}>Add Players Screen </ButtonText>
+          <ButtonIcon as={AddIcon} />
+        </Button>
 
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        marginHorizontal={20}
-      >
-        <ButtonText fontFamily={typography.fonts.poppins.medium}>Add Players Screen </ButtonText>
-        <ButtonIcon as={AddIcon} />
-      </Button>
-
-    </VStack>
+      </VStack>
+    </ScrollView>
 
   );
 };
--- client/app/theme/colors.ts
+++ client/app/theme/colors.ts
@@ -1,47 +1,13 @@
 // TODO: write documentation for colors and palette in own markdown file and add links from here
 
 const palette: Record<string, string> = {
-  // neutral100: "#FFFFFF",
-  // neutral200: "#F4F2F1",
-  // neutral300: "#D7CEC9",
-  // neutral400: "#B6ACA6",
-  // neutral500: "#978F8A",
-  // neutral600: "#564E4A",
-  // neutral700: "#3C3836",
-  // neutral800: "#191015",
-  // neutral900: "#000000",
-
-  // primary100: "#F4E0D9",
-  // primary200: "#E8C1B4",
-  // primary300: "#DDA28E",
-  // primary400: "#D28468",
-  // primary500: "#C76542",
-  // primary600: "#A54F31",
-
-  // secondary100: "#DCDDE9",
-  // secondary200: "#BCC0D6",
-  // secondary300: "#9196B9",
-  // secondary400: "#626894",
-  // secondary500: "#41476E",
-
-  // accent100: "#FFEED4",
-  // accent200: "#FFE1B2",
-  // accent300: "#FDD495",
-  // accent400: "#FBC878",
-  // accent500: "#FFBB50",
-
-  // angry100: "#F2D6CD",
-  // angry500: "#C03403",
-
-  // overlay20: "rgba(25, 16, 21, 0.2)",
-  // overlay50: "rgba(25, 16, 21, 0.5)",
-
 
   // Light --> Dark
   neutral100: "#FFFFFF",
   neutral300: "#D7CEC9",
   neutral400: "#9E9E9E",
   neutral500: "#292929",
+  neutral600: "#1D1D1D", 
   neutral900: "#000000",
 
   angry100: "#F2D6CD",
@@ -70,9 +36,13 @@ export const colors = {
    */
   textDim: palette.neutral400,
   /**
+   * The secondary color of the screen background.
+   */
+  secondaryBackground: palette.neutral500,
+  /**
    * The default color of the screen background.
    */
-  background: palette.neutral500,
+  background: palette.neutral600,
   /**
    * The default border color.
    */
