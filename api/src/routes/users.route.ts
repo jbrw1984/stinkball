e7481ca62361d28aaccc998276f74b1ff70a5217
Avery Tribbett <apatribbett03@gmail.com>
2023-12-04 21:19:29 -0600

    Get user by email and password controller

--- api/src/routes/users.route.ts
+++ api/src/routes/users.route.ts
@@ -16,6 +16,7 @@ export class UserRoute implements Routes {
   private initializeRoutes() {
     this.router.get(`${this.path}`, this.user.getUsers);
     this.router.get(`${this.path}/:id(\\d+)`, this.user.getUserById);
+    this.router.get(`${this.path}/:email/:password`, this.user.getUserByEmailPassword);
     // this.router.post(`${this.path}`, ValidationMiddleware(CreateUserDto, 'body'), this.user.createUser);
     // this.router.put(`${this.path}/:id(\\d+)`, ValidationMiddleware(CreateUserDto, 'body', true), this.user.updateUser);
     this.router.post(`${this.path}`, ValidationMiddleware(CreateUserDto), this.user.createUser);
